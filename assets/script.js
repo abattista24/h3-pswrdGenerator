ff3e47d58d3455dd68ef88e220b25362e51c58a6
Anna Battistals <anna.r.battista@gmail.com>
2020-09-25 14:13:17 -0600

    added files to get started

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click");
+
+generateBtn.addEventListener("click" + ".btn");
+prompt ("Let's create a password")
+
+
+// button click needs to trigger an alert 
+function button () {prompt("Would you like a specialized password");
+
+}
+
+function testing () {alert("why does this not work???")};
+// cycle through prompts   loop
+
+
+
+// prompt (Would you like a specialized password)
+      // yes- go on,   no-give very basic password
+
+// prompt password length (between 8 and 128) alert ("how long would you like your password?")
+
+// character types (special characters, upperCase, lower case, number)
+
+// store input data
+
+
+
+// randomize given length and using given types of chars
+
+// produce password
