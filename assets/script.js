5a29d6761901dbf4e50c90060da33189ba495f54
Anna Battistals <anna.r.battista@gmail.com>
2020-09-25 23:47:36 -0600

    added what to do with password option statements.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "1234567890";
-var special ="@%+\/'!#$^?:.(){}[]~-_.";
-
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var number = ["1234567890"];
+var specialChar =["@%+\/'!#$^?:.(){}[]~-_."];
+var picks = ""
+var genPsw =""
 
 // Write password to the #password input
 function writePassword() {
@@ -15,43 +16,53 @@ function writePassword() {
   passwordText.value = password;
 
   // button click needs to trigger an alert 
-  var start = confirm("Let's create a password");
+  // function options () { 
+    var start = confirm("Let's create a password");
 
   if (start) {
       var pswLength =
      prompt("How long would you like your password (between 8 and 128");
-       if(pswLength < 7 | pswLength > 128){
-         confirm("try again must be between 8 and 128");
-         pswLength;
-       }  else {
-          pwsLength= parseInt; 
-          var lower  = confirm("would you like a lower case letter in your password?");
+      if(pswLength < 7 || pswLength > 129){
+        confirm("try again must be between 8 and 128");
+        // if time work on getting this to return to start of    loop
+       return start;
+      } else {
+        pwsLength= parseInt; 
+        var lower  = confirm("would you like a lower case letter in your password?");
         
-          var upper =confirm ("would you like an upper case letter in your password?")
+        var upper =confirm ("would you like an upper case letter in your password?")
 
-          var num = confirm ("would you like a number in your password?")
+        var num = confirm ("would you like a number in your password?")
 
-          var special = confirm ("would you like a special character in your password?")
-          }
+        var special = confirm ("would you like a special character in your password?")
+       }
 
+       if ((lower) picks += lowerCase
+       || (upper) picks += upperCase || (nun) picks += number|| (special) picks += specialChar)) {
+        for (var i = 0; i < pswLength; i++) {
+          genPsw= Math.floor(Math.random() * picks.pswLength);
+        
+
+       } else () { 
+         genPsw=math.floor(math.random() * pswLength)
+       }
+  
+
+       
+      }
+
+    // }
   } else {
      alert("Come back later when you need a password, goodbye");
    }
   
   
-
- 
-  
-}
+ }
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click");
 
-// MY CODE STARTS HERE
-
-
 generateBtn.addEventListener("click", writePassword);
-
 
 
 
